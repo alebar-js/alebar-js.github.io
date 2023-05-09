4ee5b589e6d657513f36a85a3b9006f8aacf9fcf
Alejandro Barnola <alebarnola96@gmail.com>
2023-05-08 19:21:01 -0500

    (fix): Github Header style import

--- components/github/Header/Header.tsx
+++ components/github/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
-import styles from "./Header.module.css";
+import styles from "./styles.module.css";
 
 const Header: React.FC = () => {
   return (
